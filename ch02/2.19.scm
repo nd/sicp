fa2ecf430eecd78ed424cc73934bc67c9b5c431c
nd <dmitry.neverov@gmail.com>
2009-06-25 23:04:58 +0400

    ex. 2.19

---
+++ ch02/2.19.scm
@@ -0,0 +1,17 @@
+(define (first-denomination coins)
+  (car coins))
+
+(define (except-first-denomination coins)
+  (cdr coins))
+
+(define (no-more? coins)
+  (null? coins))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount (except-first-denomination coin-values))
+                 (cc (- amount (first-denomination coin-values))
+                     coin-values)))))
+
+(define us-coins (list 50 25 10 5 1))
