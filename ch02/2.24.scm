a80dd0449582532e10b8b2ec18526468a29d56bb
nd <dmitry.neverov@gmail.com>
2009-06-26 08:08:36 +0400

    ex. 2.24

---
+++ ch02/2.24.scm
@@ -0,0 +1,9 @@
+(list 1 (list 2 (list 3 4)))
+
+(list 1 (list 2 (cons 3 (cons 4 nil))))
+
+(list 1 (cons 2 (cons (cons 3 (cons 4 nil)) nil)))
+
+(cons 1 (cons (cons 2 (cons (cons 3 (cons 4 nil)) nil))) nil)
+
+
