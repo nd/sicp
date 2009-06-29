9ada422a402054c8c55d16d23cc1e07864799bb7
nd <dmitry.neverov@gmail.com>
2009-06-29 22:27:14 +0400

    ex. 2.36

---
+++ ch02/2.36.scm
@@ -0,0 +1,7 @@
+(load "~/projects/sicp/ch02/accumulate.scm")
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      (list)
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
