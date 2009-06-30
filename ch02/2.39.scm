afe6c2bff3052a06ae199115dc24b5c3ee7e52b0
nd <dmitry.neverov@gmail.com>
2009-06-30 22:30:07 +0400

    ex. 2.39

---
+++ ch02/2.39.scm
@@ -0,0 +1,19 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) (list) sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) (list) sequence))
