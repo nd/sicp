fc50820fd1c10234b9560fb3323f049a374f419f
nd <dmitry.neverov@gmail.com>
2009-07-07 08:52:28 +0400

    ex. 2.51

---
+++ ch02/2.51.scm
@@ -0,0 +1,28 @@
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+
+(define (below painter1 painter2)
+  (rotate270
+   (beside (rotate90 painter1)
+           (rotate90 painter2))))
+
+;;or
+
+(define (below painter1 painter2)
+  (rotate90
+   (beside (rotate270 painter1)
+           (rotate270 painter2))))
