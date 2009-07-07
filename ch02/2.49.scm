fb4a51205a04c0c4d2032ecbba10d3f3ce033778
nd <dmitry.neverov@gmail.com>
2009-07-07 08:30:20 +0400

    ex. 2.49

---
+++ ch02/2.49.scm
@@ -0,0 +1,16 @@
+;; data for painters:
+;; a
+(list (make-segment (make-vect 0 0) (make-vect 1 0))
+      (make-segment (make-vect 1 0) (make-vect 1 1))
+      (make-segment (make-vect 1 1) (make-vect 0 1))
+      (make-segment (make-vect 0 1) (make-vect 0 0)))
+
+;; b
+(list (make-segment (make-vect 0 0) (make-vect 1 1))
+      (make-segment (make-vect 1 0) (make-vect 0 1)))
+
+;; c
+(list (make-segment (make-vect 0 0.5) (make-vect 0.5 1))
+      (make-segment (make-vect 0.5 1) (make-vect 1 0.5))
+      (make-segment (make-vect 1 0.5) (make-vect 0.5 0))
+      (make-segment (make-vect 0.5 0) (make-vect 0 0.5)))
