af23cb5afe96590f3112d019fec6a244782dc486
nd <dmitry.neverov@gmail.com>
2009-07-08 21:46:45 +0400

    ex. 2.54

---
+++ ch02/2.54.scm
@@ -0,0 +1,5 @@
+(define (equal? a b)
+  (cond ((and (symbol? a) (symbol? b)) (eq? a b))
+        ((and (pair? a) (pair? b)) (and (equal? (car a) (car b))
+                                        (equal? (cdr a) (cdr b))))
+        (else false)))
