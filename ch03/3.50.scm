fbf6cdc163b5b2a08971ab1166579115c22956ea
nd <dmitry.neverov@gmail.com>
2009-09-06 11:36:14 +0400

    ex. 3.50

---
+++ ch03/3.50.scm
@@ -0,0 +1,7 @@
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (stream-cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
