15267c77b13b275f249ad0815c9a75693ad9c794
nd <dmitry.neverov@gmail.com>
2009-09-07 20:37:11 +0400

    ex. 3.60

---
+++ ch03/3.60.scm
@@ -0,0 +1,5 @@
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
+                            (mul-series (stream-cdr s1) s2))))
