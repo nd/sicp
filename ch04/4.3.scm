72b03cb63b97eb7f951b06f57cfe913783650318
nd <dmitry.neverov@gmail.com>
2009-09-23 22:52:40 +0400

    finish

--- ch04/4.3.scm
+++ ch04/4.3.scm
@@ -7,3 +7,13 @@
                  ((application? exp) (apply (eval (operator exp) env)
                                            (list-of-values (operands exp) env)))
                  (else (error "Unknown expression type -- EVAL" exp)))))))
+
+(put 'eval '(quote)  (lambda (exp env) (text-of-quotation exp)))
+(put 'eval '(set!)   (lambda (exp env) (eval-assignment exp env)))
+(put 'eval '(define) (lambda (exp env) (eval-definition exp env)))
+(put 'eval '(if)     (lambda (exp env) (eval-if exp env)))
+(put 'eval '(begin)  (lambda (exp env) (eval-sequence (begin-actions exp) env)))
+(put 'eval '(cond)   (lambda (exp env) (eval (cond->if exp) env)))
+(put 'eval '(lambda)      (lambda (exp env) (make-procedure (lambda-parameters exp)
+                                                  (lambda-body exp)
+                                                  env)))
