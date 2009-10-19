7015cd7a255b935e8efb839542a719882d06a959
nd <dmitry.neverov@gmail.com>
2009-10-19 23:13:29 +0400

    ex. 4.26

---
+++ ch04/4.26.scm
@@ -0,0 +1,16 @@
+(define (analyze exp)
+  (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
+        ...
+        ((unless? exp)              (analyze (unless->if exp)))
+        ..
+        (else (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (unless? exp) (eq? (car exp) 'unless))
+(define (unless-predicat exp) (cadr exp))
+(define (unless-consequent exp) (caddr exp))
+(define (unless-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+(define (unless->if exp)
+  (make-if (unless-predicat exp) (unless-alternative exp) (unless-consequent exp)))
